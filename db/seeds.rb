afd3991c9790c604c63d3af833ce1ccb8c1eb212
Carl Quivron <carl.quivron@gmail.com>
2017-08-11 16:14:57 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,33 @@ json = JSON.load(open(url))
 json['drinks'].each do |ingredient|
   Ingredient.new(name: ingredient['strIngredient1']).save
 end
+
+url = "https://images.unsplash.com/photo-1455621481073-d5bc1c40e3cb?ixlib=rb-0.3"
+product = Cocktail.new(name: 'Mojito')
+product.remote_photo_url = url
+product.save
+
+url = "https://images.unsplash.com/photo-1490048807374-acde8ef855cc?ixlib=rb-0.3"
+product = Cocktail.new(name: 'Tequila Sunrise')
+product.remote_photo_url = url
+product.save
+
+url = "https://images.unsplash.com/photo-1444728399417-08d2aa39e6f4?ixlib=rb-0.3"
+product = Cocktail.new(name: 'Sunset Fever')
+product.remote_photo_url = url
+product.save
+
+url = "http://www.1001cocktails.com/magazine/wp-content/uploads/ccc-300x198.png"
+product = Cocktail.new(name: 'Weeski Coca')
+product.remote_photo_url = url
+product.save
+
+url = "https://images.unsplash.com/photo-1500630967344-3b1f546423ce?ixlib=rb-0.3"
+product = Cocktail.new(name: 'Caipi')
+product.remote_photo_url = url
+product.save
+
+url = "https://cde.peru.com/ima/0/0/6/8/5/685235/611x458/lima.jpg"
+product = Cocktail.new(name: 'Maracuya')
+product.remote_photo_url = url
+product.save
